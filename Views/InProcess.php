33297090b5098e7a616009cd0e385df2c3b7692d
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-01-05 11:26:01 +0700

    Create InProcess Handling page

---
+++ Views/InProcess.php
@@ -0,0 +1,11 @@
+<!-- In Process -->
+<div class="row justify-content-center">
+    <div class="col-8">
+        <img src="../Assets/img/Developer activity-bro.svg" alt="404 NOT FOUND">
+    </div>
+</div>
+
+<!-- Buttons Back to Home -->
+<?php
+include $_SERVER['DOCUMENT_ROOT'] .  "/Parts/Button.php";
+?>

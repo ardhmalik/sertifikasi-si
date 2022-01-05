e4002f164e49d340bf4de8be285ee2949cf1ea88
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-01-05 11:25:07 +0700

    Create 404 Handling page

---
+++ Views/404.php
@@ -0,0 +1,11 @@
+<!-- 404 Not Found -->
+<div class="row justify-content-center">
+    <div class="col-8">
+        <img src="./Assets/img/404 Error-bro.svg" alt="404 NOT FOUND">
+    </div>
+</div>
+
+<!-- Buttons Back to Home -->
+<?php
+include $_SERVER['DOCUMENT_ROOT'] .  "/Parts/Button.php";
+?>

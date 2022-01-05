2f5f8aced0ee224bb4c0b55b72098216cefe300a
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-01-05 11:28:33 +0700

    Create Algorithm Implementation page

---
+++ Views/Logic.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Sorting & Searcing Page
+ */
+include $_SERVER['DOCUMENT_ROOT'] . "/Logic/index.php";
+
+/**
+ * Buttons Back to Home
+ */ 
+include $_SERVER['DOCUMENT_ROOT'] . "/Parts/Button.php";
+?>

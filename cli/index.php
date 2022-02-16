1b61b5bf30f760f01cec6d6b46e69a41ba4264ca
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-02-16 20:12:32 +0700

    Create modul input data array

--- cli/index.php
+++ cli/index.php
@@ -11,21 +11,23 @@ class Algorithm
     public function index()
     {
         echo "\n============== MENU ==============\n";
-        echo "~:| Algorithm Implementation |:~ \n";
+        echo "~:| Algorithm Implementation |:~ \n\n";
         echo "[1] Input Angka \n";
         echo "[2] Sorting \n";
         echo "[3] Searching \n";
-        echo "[4] Selesai \n";
+        echo "[4] Selesai \n\n";
         echo "Masukkan pilihan [1, 2, 3, 4] : ";
         $input = trim(fgets(STDIN));
+        echo "----------------------------------\n";
 
         switch ($input) {
             case 1:
                 $this->input_angka();
                 break;
             case 4:
-                $this->print_array($this->new_array);
                 echo "\n>>>>>>>>> KELUAR PROGRAM >>>>>>>>>";
+                var_dump($this->new_array);
+                die;
                 break;
             default:
                 $this->index();
@@ -35,16 +37,25 @@ class Algorithm
 
     public function input_angka()
     {
-        echo "\n========== INPUT_ANGKA ==========\n";
+        if ($this->new_array == !null) {
+            echo "Data sudah tersedia!\n";
+            $this->index();
+        }
+
+        echo "\n========== INPUT ANGKA ==========\n";
         echo "Masukkan jumlah nilai tugas: ";
-        
         $qty = trim(fgets(STDIN));
 
-        if ($qty <= 0 ) {
-            echo "Angka harus lebih dari 0!\n";
-            $this->input_angka();
-        } elseif ($qty > 10) {
-            echo "Maksimal 10 angka!\n";
+        if (is_numeric($qty)) {
+            if ($qty <= 1) {
+                echo "Minimal 2 nilai!\n";
+                $this->input_angka();
+            } elseif ($qty > 10) {
+                echo "Maksimal 10 nilai!\n";
+                $this->input_angka();
+            }
+        } else {
+            echo "Masukkan bilangan bulat!\n";
             $this->input_angka();
         }
 
@@ -55,31 +66,27 @@ class Algorithm
             echo "Angka ke-$no : ";
             $int = trim(fgets(STDIN));
 
-            if (strlen($int) <= 2) {
-                array_push($this->new_array, $int);
-            } else {
-                echo "Masukkan angka dua digit!\n";
+            if ($int == null) {
+                echo "Masukkan angka minimal satu digit!\n\n";
                 $i -= 1;
+            } else {
+                if ($int == 0) {
+                    echo "Masukkan angka lebih dari 0!\n\n";
+                    $i -= 1;
+                } elseif (strlen($int) > 2) {
+                    echo "Masukkan angka maksimal dua digit!\n\n";
+                    $i -= 1;
+                } else {
+                    array_push($this->new_array, $int);
+               }
             }
         }
 
-        // $qty = 0;
-
         $this->index();
-        // var_dump($this->new_array);
-        // die;
-    }
-
-    public function print_array($array)
-    {
-        $arr = implode(", ", $array);
-        echo "[ $arr ]";
     }
 }
 
 $algorithm = new Algorithm();
 $algorithm->index();
-
-
 
 ?>
